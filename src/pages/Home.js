240630abe8836e92bd1da5fe5757bc51d8d5e281
paszo <paszo2008@gmail.com>
2023-06-04 17:43:33 +0200

    version pre 6.4

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div>
+            <p>Home</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aut consectetur consequuntur eaque esse et excepturi exercitationem facere facilis laudantium omnis praesentium, quasi quia quo recusandae rem saepe tenetur, totam!</p>
+        </div>
+    )
+}
